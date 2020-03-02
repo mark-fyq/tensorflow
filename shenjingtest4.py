bfaa3bb2668b3f7e003fff15af7c392d08023832
mark <741114622@qq.com>
2020-03-02 19:22:50 +0800

    test4

---
+++ shenjingtest4.py
@@ -0,0 +1,13 @@
+#shenjingtest5.py
+import tensorflow.compat.v1 as tf
+
+tf.disable_eager_execution()
+
+input1 = tf.placeholder(tf.float32)
+
+input2 = tf.placeholder(tf.float32)
+
+output = tf.multiply(input1,input2)
+
+with tf.Session() as sess:
+    print(sess.run(output,feed_dict={input1:[7.],input2:[2.]}))
